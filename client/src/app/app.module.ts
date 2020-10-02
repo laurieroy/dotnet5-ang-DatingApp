447393d89a81754b13adfc5ce4b17b439045a372
Laurie Roy <laurieroy.dev@gmail.com>
2020-10-02 10:49:57 -0600

    add route guards, routing, shared module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -31,12 +32,9 @@ import { MessagesComponent } from './messages/messages.component';
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
-    }),
     HttpClientModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
