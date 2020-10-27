7d4732358058c819e9ac5ff961f54c4c7f591781
Laurie Roy <laurieroy.dev@gmail.com>
2020-10-27 12:49:10 -0600

    Change Main to IsMain photo

---
+++ API/Data/Migrations/20201027184811_updatePhotoIsMain.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class updatePhotoIsMain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Main",
+                table: "Photos",
+                newName: "IsMain");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMain",
+                table: "Photos",
+                newName: "Main");
+        }
+    }
+}
