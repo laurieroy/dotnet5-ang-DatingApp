fe28b2c32fce09240f8189619bc0979cd4ffb0ba
Laurie Roy <laurieroy.dev@gmail.com>
2020-11-01 09:31:22 -0700

    Add edit form, edit route to nav dropdown

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
 import { Member } from 'src/app/models/member';
 import { User } from 'src/app/models/user';
 import { AccountService } from 'src/app/_services/account.service';
@@ -10,20 +13,29 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
+
   constructor(private accountService: AccountService,
-              private memberService: MembersService) {
+              private memberService: MembersService,
+              private toastr: ToastrService) {
                 this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
                }
 
   ngOnInit(): void {
-    this.loadMember();
+    this.loadMember();``
   }
-  
+
   loadMember() {
     this.memberService.getMember(this.user.username).subscribe(member => {
       this.member = member;
-    })
+    });
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully!');
+    this.editForm.reset(this.member);
   }
 }
