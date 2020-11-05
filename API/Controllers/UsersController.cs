a41c72b04ad547012e52c99c7766ac15378f374c
Laurie Roy <laurieroy.dev@gmail.com>
2020-11-05 13:56:43 -0700

    Add reactive form, validation

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -107,17 +107,22 @@ namespace API.Controllers
     public async Task<ActionResult> DeletePhoto(int photoId)
     {
       var user = await _userRepository.GetUserByUsernameAsync(User.GetUserName());
-      var photo = await user.Photos.FirstOrDefault(x => x.Id == photoId);
+      var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
 
       if (photo == null) return NotFound();
-      if (photoId == IsMain) return BadRequest("You cannot delete your main image. Set a different photo to main, or remove flag and try again.");
+
+      if (photo.IsMain) return BadRequest("You cannot delete your main image. Set a different photo to main, or remove flag and try again.");
+      
       if (photo.PublicId != null)
       {
         var result = await _photoService.DeletePhotoAsync(photo.PublicId);
         if (result.Error != null) return BadRequest(result.Error.Message);
       }
+      
       user.Photos.Remove(photo);
+      
       if (await _userRepository.SaveAllAsync()) return Ok();
+
       return BadRequest("Failed to delete photo.");
     }
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,7 +40,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     ServerErrorComponent,
     MemberCardComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    ReactiveFormsModule
   ],
   imports: [
     FormsModule,
