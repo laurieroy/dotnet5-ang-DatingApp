8af4d4e4c6195d424a37383008b9ee826969faa2
Laurie Roy <laurieroy.dev@gmail.com>
2020-11-11 10:49:42 -0700

    change caching of user settings

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -17,33 +17,32 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination;
   userParams: UserParams;
   user: User;
-  genderList = [{value: 'female', display: 'Females'}, {value: 'male', display: 'Males'}]
-  constructor(private memberService: MembersService, private accountService:
-    AccountService) {
-      this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
-        this.user = user;
-        this.userParams = new UserParams(user);
-      });
-    }
+  genderList = [{value: 'female', display: 'Females'}, {value: 'male', display: 'Males'}];
+  constructor(private memberService: MembersService) {
+    this.userParams = this.memberService.getUserParams();
+  }
 
   ngOnInit(): void {
     this.loadMembers();
   }
 
   loadMembers() {
+    this.memberService.setUserParams(this.userParams);
     this.memberService.getMembers(this.userParams).subscribe(response => {
       this.members = response.result;
       this.pagination = response.pagination;
+
     });
   }
 
   pageChanged(event: any) {
     this.userParams.pageNumber = event.page;
+    this.memberService.setUserParams(this.userParams);
     this.loadMembers();
   }
 
   resetFilters() {
-    this.userParams = new UserParams(this.user);
+    this.userParams = this.memberService.resetUserParams();
     this.loadMembers();
   }
 
