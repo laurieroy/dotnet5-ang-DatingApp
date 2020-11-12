cb484e551ca9e4911c9103d6225c7a9cd2ba871c
Laurie Roy <laurieroy.dev@gmail.com>
2020-11-12 09:54:09 -0700

    Add likes entity and repo

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,8 +1,6 @@
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
-
-
 namespace API.Data
 {
     public class DataContext : DbContext
@@ -10,5 +8,25 @@ namespace API.Data
         public DataContext(DbContextOptions options) : base(options) { }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+            builder.Entity<UserLike>()
+                .HasKey(k => new {k.SourceUserId, k.LikedUserId});
+            
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
