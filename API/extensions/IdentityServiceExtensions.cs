83e1815b11da221fab521864d0e91ccb133efb68
Laurie Roy <laurieroy.dev@gmail.com>
2020-11-18 11:41:07 -0700

    Add policy

--- API/extensions/IdentityServiceExtensions.cs
+++ API/extensions/IdentityServiceExtensions.cs
@@ -31,6 +31,15 @@ namespace API.extensions
                         ValidateAudience = false,
                     };
                 });
+            
+            services.AddAuthorization(opt => 
+            {
+                opt.AddPolicy("RequireAdminRole", policy => 
+                    policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => 
+                    policy.RequireRole("Admin", "Moderator"));
+            });
+            
             return services;
         }
     }
