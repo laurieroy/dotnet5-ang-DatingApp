53db680e5321ceeed29e2c254c3fb2af7f0b4c77
Laurie Roy <laurieroy.dev@gmail.com>
2020-11-19 14:39:21 -0700

    Add SignalR, server side auth

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,6 @@
 using API.extensions;
 using API.Middleware;
+using API.SignalR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -24,6 +25,7 @@ namespace API
             services.AddControllers ();
             services.AddCors();
             services.AddIdentityServices(_config);
+            services.AddSignalR();
             services.AddSwaggerGen (c =>
             {
                 c.SwaggerDoc ("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -39,7 +41,9 @@ namespace API
 
             app.UseRouting ();
 
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod()
+            app.UseCors(policy => policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
                 .WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
@@ -49,6 +53,7 @@ namespace API
             app.UseEndpoints (endpoints =>
             {
                 endpoints.MapControllers ();
+                endpoints.MapHub<PresenceHub>("hubs/presence");
             });
         }
     }
