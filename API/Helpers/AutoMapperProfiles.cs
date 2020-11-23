3442bdbb7773e281853e0d0161affebeec4e511c
Laurie Roy <laurieroy.dev@gmail.com>
2020-11-23 10:52:34 -0700

    update time to UTC

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,29 +1,31 @@
+using System;
 using System.Linq;
 using API.DTOs;
-using API.Entities;
 using API.extensions;
+using API.Entities;
 using AutoMapper;
 
 namespace API.Helpers
 {
     public class AutoMapperProfiles : Profile
     {
-    public AutoMapperProfiles()
-    {
-        CreateMap<AppUser, MemberDto>()
-            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(
-                src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
-            .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
-        CreateMap<Photo, PhotoDto>();
-        CreateMap<MemberUpdateDto, AppUser>();
-        CreateMap<RegisterDto, AppUser>();
-        CreateMap<Message, MessageDto>()
-            .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src =>
-                src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
-            .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src =>
-                src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
-        
-    }
- 
+        public AutoMapperProfiles ()
+        {
+            CreateMap<AppUser, MemberDto> ()
+                .ForMember (dest => dest.PhotoUrl, opt => opt.MapFrom (
+                    src => src.Photos.FirstOrDefault (x => x.IsMain).Url))
+                .ForMember (dest => dest.Age, opt => opt.MapFrom (src => src.DateOfBirth.CalculateAge ()));
+            CreateMap<Photo, PhotoDto> ();
+            CreateMap<MemberUpdateDto, AppUser> ();
+            CreateMap<RegisterDto, AppUser> ();
+            CreateMap<Message, MessageDto> ()
+                .ForMember (dest => dest.SenderPhotoUrl, opt => opt.MapFrom (src =>
+                    src.Sender.Photos.FirstOrDefault (x => x.IsMain).Url))
+                .ForMember (dest => dest.RecipientPhotoUrl, opt => opt.MapFrom (src =>
+                    src.Recipient.Photos.FirstOrDefault (x => x.IsMain).Url));
+            CreateMap<DateTime, DateTime> ().ConstructUsing (d => DateTime.SpecifyKind (d, DateTimeKind.Utc));
+
+        }
+
     }
 }
